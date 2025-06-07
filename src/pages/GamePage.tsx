04b5097cdfc57ef5771556bcccb9b42a5efd058b
Timur Aizatvafin <aiztim@Timurs-MacBook-Pro.local>
2025-06-07 12:31:16 +0800

    add expandable text

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,19 +1,28 @@
 import { useParams } from 'react-router-dom';
-import { Box, Heading, Spinner, Text } from '@chakra-ui/react';
+import { Box, Button, Heading, Spinner, Text } from '@chakra-ui/react';
 import useGame from '../hooks/useGame';
+import { useState } from 'react';
 
 const GamePage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
+  const [isTrimmed, setIsTrimmed] = useState(true);
 
   if (isLoading) return <Spinner />;
   if (error) return <Text>Error: {error.message}</Text>;
   if (!game) return <Text>No game found</Text>;
 
+  const trimmedDescription = isTrimmed
+    ? game.description_raw.substring(0, 300)
+    : game.description_raw;
+
   return (
     <Box p={5}>
       <Heading>{game?.name}</Heading>
-      <Text>{game?.description_raw}</Text>
+      <Text>{trimmedDescription}</Text>
+      <Button onClick={() => setIsTrimmed(!isTrimmed)}>
+        {isTrimmed ? 'Show more' : 'Show less'}
+      </Button>
     </Box>
   );
 };
